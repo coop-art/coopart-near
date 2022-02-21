86cb6326aa21dc8ae521a973e1d633f052a77e71
coop-art <coopart@protonmail.com>
2022-02-21 23:14:28 +0100

    Deploy on testnet

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME ||'coopart-near'
+const CONTRACT_NAME = process.env.CONTRACT_NAME ||'coopart-near.coopart.testnet'
 
 function getConfig(env) {
   switch (env) {
